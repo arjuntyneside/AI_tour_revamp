e3b7bba25b331855694b54569e71fcf2dbba151e
sustains <arjuncrevathi@gmail.com>
2025-07-25 13:49:58 +0400

    Made the skeleton

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('tours/', views.tours, name='tours'),
+    path('customers/', views.customers, name='customers'),
+    path('settings/', views.settings, name='settings'),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,37 @@
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.shortcuts import redirect
+from django.contrib import messages
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def dashboard(request):
     return render(request, 'core/dashboard.html')
 
+@login_required
 def tours(request):
     return render(request, 'core/tours.html')
+
+@login_required
+def customers(request):
+    return render(request, 'core/customers.html')
+
+@login_required
+def settings(request):
+    return render(request, 'core/settings.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid username or password')
+    return render(request, 'core/login.html')
+
+def logout(request):
+    auth_logout(request)
+    return redirect('login')
